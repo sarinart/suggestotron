918593bfa9e87397079817e03dfad24e04b5ebe8
sarinart <sarinart@gmail.com>
2016-09-24 16:31:35 -0400

    final changes railsbridge

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   	resources :topics do
   		member do
   			post 'upvote'
+  			delete 'downvote'
   		end
   	end
-  	
+  	get '/about' => 'pages#about', as: :about
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
